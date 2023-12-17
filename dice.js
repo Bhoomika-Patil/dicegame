b6f3221cfbf47272a89ff86a3a8c716f98db4b9e
Bhoomika-Patil <137794416+Bhoomika-Patil@users.noreply.github.com>
2023-12-17 16:26:30 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,57 @@
+
+var random1=Math.floor((Math.random()*6)+1);
+if(random1===1){
+    document.querySelector(".img1").setAttribute("src","./images/dice1.png");
+}
+else if(random1===2){
+    document.querySelector(".img1").setAttribute("src","./images/dice2.png");
+
+}
+else if(random1===3){
+    document.querySelector(".img1").setAttribute("src","./images/dice3.png");
+
+}
+else if(random1===4){
+    document.querySelector(".img1").setAttribute("src","./images/dice4.png");
+
+}
+else if(random1===5){
+    document.querySelector(".img1").setAttribute("src","./images/dice5.png");
+
+}
+else{
+    document.querySelector(".img1").setAttribute("src","./images/dice6.png");
+}
+
+var random2=Math.floor((Math.random()*6)+1);
+if(random2===1){
+    document.querySelector(".img2").setAttribute("src","./images/dice1.png");
+}
+else if(random2===2){
+    document.querySelector(".img2").setAttribute("src","./images/dice2.png");
+
+}
+else if(random2===3){
+    document.querySelector(".img2").setAttribute("src","./images/dice3.png");
+
+}
+else if(random2===4){
+    document.querySelector(".img2").setAttribute("src","./images/dice4.png");
+
+}
+else if(random2===5){
+    document.querySelector(".img2").setAttribute("src","./images/dice5.png");
+
+}
+else{
+    document.querySelector(".img2").setAttribute("src","./images/dice6.png");
+}
+
+if(random1>random2){
+    document.querySelector("h1").textContent="🚩Player 1 is the winner";
+}
+else if(random2>random1){
+    document.querySelector("h1").textContent="Player 2 is the winner🚩";
+}else{
+    document.querySelector("h1").textContent="🚩Its a tie🚩";
+}
